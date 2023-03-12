5f7a1264f2f9a53bcbc65b482ea29bc661fddb0f
Pratyush Singh <aries.pratyush@gmail.com>
2023-03-12 12:01:38 +0530

    JpaRepository used to return data in json format

--- src/main/java/com/bootjpa/demo/controller/AlienController.java
+++ src/main/java/com/bootjpa/demo/controller/AlienController.java
@@ -1,10 +1,13 @@
 package com.bootjpa.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bootjpa.demo.dao.AlienRepo;
 import com.bootjpa.demo.model.Alien;
@@ -24,15 +27,32 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@RequestMapping("/getAlien")
-	public ModelAndView getAlien(@RequestParam int aid) {
-		ModelAndView mv=new ModelAndView();
-		mv.setViewName("showAlien.jsp");
-		Alien alien=repo.findById(aid).orElse(new Alien());
-		System.out.println(repo.findByTech("Java"));
-		mv.addObject(alien);
-		return mv;
+//	@RequestMapping("/getAlien")
+//	public ModelAndView getAlien(@RequestParam int aid) {
+//		ModelAndView mv=new ModelAndView();
+//		mv.setViewName("showAlien.jsp");
+//		Alien alien=repo.findById(aid).orElse(new Alien());
+////		System.out.println(repo.findByTech("Java"));
+//		mv.addObject(alien);
+////		Map<String,Object> map = mv.getModel();
+//////		for(String i : map.keySet()) {
+//////			System.out.println(i+" ");
+//////		}
+////		System.out.println(map.values().toString());
+//		return mv;
+//	}
+	
+	
+	@RequestMapping("/getAliens")
+	@ResponseBody
+	public List<Alien> getAliens() {
+		return repo.findAll(); //return data in json format
 	}
+	@RequestMapping("/getAliens/{aid}")
+	@ResponseBody
+	public Optional<Alien> getAlien(@PathVariable("aid")int aid) {
+		return repo.findById(aid); //returns data in json format
+	} 
 } 
 
 
--- src/main/java/com/bootjpa/demo/dao/AlienRepo.java
+++ src/main/java/com/bootjpa/demo/dao/AlienRepo.java
@@ -1,11 +1,10 @@
 package com.bootjpa.demo.dao;
 
-import java.util.*;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bootjpa.demo.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer> {
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
 	
 	
 //	List<Alien> findByTech(String tech);
